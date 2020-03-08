8bd57d0f86aef8734350242525f609d8750b0209
Minho Park <pmh9960@korea.ac.kr>
2020-03-08 22:54:24 +0900

    Fix detail in preprocessing image

--- source/preprocessing/preprocessing_image.py
+++ source/preprocessing/preprocessing_image.py
@@ -8,7 +8,7 @@ file_list = os.listdir("sample_images")
 file_list_jpg = [file for file in file_list if file.endswith(".jpg")]
 # print(file_list_jpg)
 
-num_pxs = (128, 256)
+num_pxs = (128, 256, 512)
 
 for image_name in file_list_jpg:
     for num_px in num_pxs:
@@ -16,6 +16,7 @@ for image_name in file_list_jpg:
 
         # pip install scipy==1.1.0 (need)
         image = Image.open(fname)
+        image = image.convert("RGB")  # OSError: cannot write mode LA as JPEG
 
         margin = int(abs(image.size[0] - image.size[1]) / 2)
         crop_image = (
